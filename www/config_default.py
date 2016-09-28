ce07164a080397a21c2d228c043d7c17e4703bff
Alex Wang <alexwang09@hotmail.com>
2016-09-28 23:55:09 +0800

    add configurations

---
+++ www/config_default.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Alex Wang'
+
+'''
+Default configurations.
+开发环境的标准配置
+'''
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'password': 'wxy6772102',
+        'database': 'awesome'
+    },
+    'session': {
+        'secert': 'AwEsOmE'
+    }
+}
