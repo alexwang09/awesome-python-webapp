ca2ca878450c0271cf505a2aaff3af7b3d4c4b1a
Alex Wang <alexwang09@hotmail.com>
2016-10-05 16:20:49 +0800

    add register,signin page,add session support

--- www/wsgiapp.py
+++ www/wsgiapp.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__author__ = 'Alex Wang'
+__author__ = 'Michael Liao'
+
+'''
+A WSGI application entry.
+'''
 
 import logging; logging.basicConfig(level=logging.INFO)
-import os
 
 import os, time
 from datetime import datetime
+
 from transwarp import db
 from transwarp.web import WSGIApplication, Jinja2TemplateEngine
 
@@ -26,25 +30,22 @@ def datetime_filter(t):
     dt = datetime.fromtimestamp(t)
     return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
 
-# 初始化数据库
+# init db:
 db.create_engine(**configs.db)
 
+# init wsgi app:
 wsgi = WSGIApplication(os.path.dirname(os.path.abspath(__file__)))
-# os.path.abspath(__file__) 文件的绝对路径
-# os.path.dirname(os.path.abspath(__file__)) 文件的父路径
-# 初始化jinja2模板引擎
+
 template_engine = Jinja2TemplateEngine(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'))
-# os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates') 结合多个路径
 template_engine.add_filter('datetime', datetime_filter)
 
 wsgi.template_engine = template_engine
 
-
 import urls
+
+wsgi.add_interceptor(urls.user_interceptor)
+wsgi.add_interceptor(urls.manage_interceptor)
 wsgi.add_module(urls)
 
-#
 if __name__ == '__main__':
     wsgi.run(9000, host='0.0.0.0')
-else:
-    application = wsgi.get_wsgi_application()
