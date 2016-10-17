f262cbedf867816c139e29ff1f3d43168d6a7cb6
Alex Wang <alexwang09@hotmail.com>
2016-10-17 23:49:04 +0800

    complete all pages and apis

--- www/models.py
+++ www/models.py
@@ -3,16 +3,17 @@
 
 __author__ = 'Alex Wang'
 
-
 '''
 Models for user, blog, comment.
 '''
-
 
 import time, uuid
 
 from transwarp.db import next_id
 from transwarp.orm import Model, StringField, BooleanField, FloatField, TextField
+
+def next_id():
+    return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
 
 class User(Model):
     __table__ = 'users'
